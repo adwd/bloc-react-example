9ed6c8a4c577c537e89245c7207101890baa4e3c
adwd <ecdt.mn@gmail.com>
2018-05-24 15:00:36 +0900

    fix theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import './App.css';
 import MenuAppBar from './components/MenuAppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
 
 class App extends React.Component {
   public render() {
     return (
-      <>
+      <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <MenuAppBar />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-      </>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/MenuAppBar.tsx
+++ src/components/MenuAppBar.tsx
@@ -4,10 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 
 const styles = {
   root: {
@@ -26,85 +23,30 @@ interface Props {
   classes: any;
 }
 
-interface State {
-  auth: boolean;
-  anchorEl: HTMLElement | undefined;
-}
-
-class MenuAppBar extends React.Component<Props, State> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      auth: true,
-      anchorEl: undefined,
-    };
-  }
-
-  handleChange = (event: any, checked: boolean) => {
-    this.setState({ auth: checked });
-  };
-
-  handleMenu = (event: any) => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: undefined });
-  };
-
+class MenuAppBar extends React.Component<Props> {
   render() {
     const { classes } = this.props;
-    const { auth, anchorEl } = this.state;
-    const open = Boolean(anchorEl);
 
     return (
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <MenuIcon />
-            </IconButton>
             <Typography
               variant="title"
               color="inherit"
               className={classes.flex}
             >
-              Title
+              Bloc
             </Typography>
-            {auth && (
-              <div>
-                <IconButton
-                  aria-owns={open ? 'menu-appbar' : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={this.handleClose}
-                >
-                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                </Menu>
-              </div>
-            )}
+            <div>
+              <IconButton
+                aria-haspopup="true"
+                onClick={console.log}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
           </Toolbar>
         </AppBar>
       </div>
